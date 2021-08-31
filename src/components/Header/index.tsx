0ee962cc3dc305b7f8c4a10f95a247ce0e442747
matheus97sm <matheus97sm@gmail.com>
2021-08-31 13:45:20 -0300

    Ajustes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import { AzureAD, AuthenticationState } from 'react-aad-msal';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { authProvider } from '../../msalConfig'
 
@@ -54,32 +52,3 @@ export function Header() {
     </AzureAD>
   )
 }
-
-
-{/* <AzureAD provider={authProvider} forceLogin={false}>
-{
-  ({login, logout, authenticationState, error, accountInfo}: AuthenticationProps) => {
-    switch (authenticationState) {
-      case AuthenticationState.Authenticated:
-        return (
-          <p>
-            <span>Welcome, {accountInfo.account.name}!</span>
-            <button onClick={() => logout()}>Logout</button>
-          </p>
-        );
-      case AuthenticationState.Unauthenticated:
-        return (
-          <div>
-            {error && <p><span>An error occured during authentication, please try again!</span></p>}
-            <p>
-              <span>Hey stranger, you look new!</span>
-              <button onClick={() => login()}>Login</button>
-            </p>
-          </div>
-        );
-      case AuthenticationState.InProgress:
-        return (<p>Authenticating...</p>);
-    }
-  } 
-}
-</AzureAD> */}
